a4a820e829f46d5e26a259c5ace4edb3229f51dd
Shreyash Singh <65494781+shreyash001@users.noreply.github.com>
2023-05-07 20:03:23 +0530

    Update

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const fs = require('fs')
+
+const NewsDetails = require("./model/headline.model.js")
+
+
+const app = express()
+app.use(express.json())
+app.use(cors)
+
+app.get("/",(req,res) => {
+    res.send("server running")
+})
+
+
+const data = JSON.parse(fs.readFileSync("./jsondata.json","utf-8"))
+// console.log(data);
+
+//Now to import the data from .json file to mongoDB
+const importData = async() => {
+    await NewsDetails.create(data)
+    process.exit()
+}
+// run this above function once for importing the data to Database
+// importData()
+
+const PORT = 5000
+app.listen(PORT, () => {
+    console.log(`Listing to port ${PORT}`)
+})
+
+const CONNECTION_URL = "mongodb+srv://admin:admin@cluster0.9msr4gt.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(CONNECTION_URL).then( () => {
+    console.log("Connected to database")
+}).catch((error) =>{
+    console.log(error)
+})
